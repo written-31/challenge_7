8322d038873d9bde97594c3390e099dcc9ffcb1e
written-31 <115435294+written-31@users.noreply.github.com>
2023-10-29 22:10:03 +0900

    アニメーション完成

--- dist/js/script.js
+++ dist/js/script.js
@@ -101,6 +101,45 @@ jQuery(function ($) {
         },
       },
     });
+
+
+    $(document).ready(function() {
+      // 最初は要素を非表示にする
+      $('.p-header').hide();
+  
+      // 2秒後に要素を表示する
+      setTimeout(function() {
+          $('.p-header').show(); // 要素を表示する
+      }, 1100); // 2秒後に実行する（2000ミリ秒 = 2秒）
+  });
+
+
+
+    // コンテンツをふわっと表示
+  // 動きのきっかけとなるアニメーションの名前を定義
+  function fadeAnime() {
+    // ふわっ
+    $(".js-fadeUpTrigger").each(function () {
+      //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top - 50; //要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass("u-fadeUp"); // 画面内に入ったらfadeUpというクラス名を追記
+      } else {
+        $(this).removeClass("u-fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
+      }
+    });
+  }
+
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+  }); // ここまで画面をスクロールをしたら動かしたい場合の記述
+
+
+
+
 });
 
 
